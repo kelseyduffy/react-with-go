68e86ba6b4b2c43d4b72b7f49012f9c1ca4deb3d
kelseyduffy <kduffy@osisoft.com>
2022-07-13 23:05:58 -0400

    appheader and props

--- go-react-1/src/index.js
+++ go-react-1/src/index.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import AppFooter from './AppFooter';
 import AppContent from './AppContent';
+import AppHeader from './AppHeader';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 
 class App extends Component {
@@ -11,10 +13,8 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div>
-          <h1>Hello, world!</h1>
-          <AppContent />
-        </div>
+        <AppHeader title="Cool App"/>
+        <AppContent />
         <AppFooter />
       </div>
     );
