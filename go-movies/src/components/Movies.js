525ee8abf11287cd3a0f70c51e17db90cde276f9
kelseyduffy <kduffy@osisoft.com>
2022-07-21 17:18:24 -0400

    added error handling for api call from react

--- go-movies/src/components/Movies.js
+++ go-movies/src/components/Movies.js
@@ -3,32 +3,60 @@ import { Link } from 'react-router-dom';
 
 export default class Movies extends Component {
 
-    state = { movies: [] };
+    state = { 
+        movies: [],
+        isLoaded: false,
+        error: null,
+    };
 
     // Executed after the component is rendered to the screen
     componentDidMount() {
-        this.setState({
-            movies: [
-                { id: 1, title: "The Shawshank Redemption", runtime: 142 },
-                { id: 2, title: "The Godfather", runtime: 175 },
-                { id: 3, title: "The Dark Knight", runtime: 153 },
-            ]
-        })
+        fetch("http://localhost:4000/v1/moviesx")
+            // .then((response) => response.json())
+            .then((response) => {
+                console.log("status code is", response.status);
+                if (response.status !== "200") {
+                    let err = Error;
+                    err.message = "Invalid response code: " + response.status;
+                    this.setState({error: err});
+                }
+                return response.json();
+            })
+            .then((json) => {
+                this.setState({
+                    movies: json.movies,
+                    isLoaded: true,
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    })
+                });
+            });
     }
 
     render() {
-        return (
-            <Fragment>
-                <h2>Choose a movie</h2>
+        const { movies, isLoaded, error } = this.state;
 
-                <ul>
-                    { this.state.movies.map( (m) => (
-                        <li key={m.id}>
-                            <Link to= {`/movies/${m.id}`}>{m.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </Fragment>
-        );
+        if (error) {
+            return <div>Error: {error.message} </div>
+        } else if (!isLoaded) {
+            return <p>Loading...</p>
+        } else {
+            return (
+                <Fragment>
+                    <h2>Choose a movie</h2>
+
+                    <ul>
+                        { this.state.movies.map( (m) => (
+                            <li key={m.id}>
+                                <Link to= {`/movies/${m.id}`}>{m.title}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                </Fragment>
+            );
+        } 
     }
 }
