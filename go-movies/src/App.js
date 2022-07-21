94830f08b3605951251ff0b874dafbfcd6195eae
kelseyduffy <kduffy@osisoft.com>
2022-07-21 17:49:19 -0400

    refactor Genres tab to pull from api

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch } from 'react-router-dom';
-import Categories from './components/Categories';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Movies from './components/Movies';
 import Home from './components/Home';
 import Admin from './components/Admin';
 import OneMovie from './components/OneMovie';
+import Genres from './components/Genres';
 
 export default function App() {
   return (
@@ -28,7 +28,7 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
-                  <Link to="/by-category">Movies by Category</Link>
+                  <Link to="/genres">Genres</Link>
                 </li>
                 <li className="list-group-item">
                   <Link to="/admin">Manage Catalog</Link>
@@ -45,48 +45,23 @@ export default function App() {
               <Route path="/movies">
                 <Movies />
               </Route>
-              <Route exact path="/by-category">
-                <CategoryPage />
+
+              <Route exact path="/genres">
+                <Genres />
               </Route>
-
-              <Route
-              exact
-              path="/by-category/Drama"
-              render={(props) => <Categories {...props} title={`Drama`} />}
-              />
-
-              <Route
-              exact
-              path="/by-category/Comedy"
-              render={(props) => <Categories {...props} title={`Comedy`} />}
-              />
 
               <Route path="/admin">
                 <Admin />
               </Route>
+
               <Route path="/">
                 <Home />
               </Route>
+
             </Switch>
           </div>
         </div>
       </div>
     </Router>
-  );
-}
-
-function CategoryPage() {
-  
-  let { path, url } = useRouteMatch();
-  
-  return (
-    <div>
-      <h2>Categories</h2>
-
-      <ul>
-        <li><Link to={`${path}/drama`}>Drama</Link> </li>
-        <li><Link to={`${url}/comedy`}>Comedy</Link> </li>
-      </ul>
-    </div>
   );
 }
