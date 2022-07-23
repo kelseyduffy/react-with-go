922a10e6b6d7a7be805ebb5ef2519cc4b75eba69
kelseyduffy <kduffy@osisoft.com>
2022-07-22 22:45:38 -0400

    add client-side feedback from edit movie

--- go-movies/src/components/EditMovie.js
+++ go-movies/src/components/EditMovie.js
@@ -4,6 +4,7 @@ import './EditMovie.css';
 import Input from './form-components/Input';
 import Textarea from './form-components/Textarea';
 import Select from './form-components/Select';
+import Alert from './ui-components/Alert';
 
 export default class EditMovie extends Component {
 
@@ -29,6 +30,10 @@ export default class EditMovie extends Component {
             isLoaded: false,
             error: null,
             errors: [],
+            alert: {
+                type: "d-none",
+                message: ""
+            }
         }
 
         this.handleChange = this.handleChange.bind(this);
@@ -62,7 +67,15 @@ export default class EditMovie extends Component {
         fetch('http://localhost:4000/v1/admin/editmovie', requestOptions)
             .then(response => response.json())
             .then(data => {
-                console.log(data);
+                if (data.error) {
+                    this.setState({
+                        alert: { type: "alert-danger", message: data.error.message }
+                    })
+                } else {
+                    this.setState({
+                        alert: { type: "alert-success", message: "Changes saved!" }
+                    })
+                }
             })
     }
 
@@ -133,6 +146,10 @@ export default class EditMovie extends Component {
             return (
                 <Fragment>
                     <h2>Add/Edit Movie</h2>
+                    <Alert 
+                        alertType={this.state.alert.type}
+                        alertMessage={this.state.alert.message}
+                    />
                     <hr />
                     <form onSubmit={this.handleSubmit}>
                         <input
@@ -193,10 +210,6 @@ export default class EditMovie extends Component {
 
                         <button className="btn btn-primary">Save</button>
                     </form>
-
-                    <div className="mt-3">
-                        <pre>{JSON.stringify(this.state, null, 3)}</pre>
-                    </div>
                 </Fragment>
             );
         }
